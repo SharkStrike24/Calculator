0044e438e851079cff87fb63500b0ddf462cd98a
Austin McCartney <amacfcd@gmail.com>
2022-06-27 10:15:31 -0500

    finished button functions for numbers

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-function drawButtons(){
-	const buttonContainer = document.getElementById("button-container")
-	for(let i = 0; i < 10; i++){
-		let button = document.createElement('button')
-		button.classList.add("number-button")
-		terms.setAttribute("id",i.toString())
-		buttonContainer.appendChild(button)
+//Create a javascript program to imitate a calculator
+let operation = ""
+function createButton(className, id, text, clickFunction, parent){
+	let button = document.createElement("button")
+	button.classList.add(className.toString())
+	button.setAttribute("id", id.toString())
+	button.innerHTML = text.toString()
+	button.addEventListener("click", clickFunction)
+	if(parent != undefined){
+		parent.appendChild(button)
 	}
 }
+
+
+function drawButtons(){
+	const textDisplay = document.getElementById("text-display") 
+	const buttonContainer = document.getElementById("button-container")
+	console.log(buttonContainer)
+	//0 button is in an unusual place due to the nature of this for loop
+	for(let i = 0; i < 10; i++){
+		createButton("number-button", i, i, () => {
+			operation += i.toString()
+			operation = parseInt(operation).toString()
+			textDisplay.innerHTML = operation
+			console.log(operation)
+		}, buttonContainer)
+	}
+
+}
+
+drawButtons()
