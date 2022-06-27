385d3c2240db057a782157fa3f81a3a2648b49e1
Austin McCartney <amacfcd@gmail.com>
2022-06-27 11:46:05 -0500

    more stuff finished

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Create a javascript program to imitate a calculator
 let operation = ""
-function createButton(className, id, text, clickFunction, parent){
+function createButton(className, id, text, parent, clickFunction){
 	let button = document.createElement("button")
 	button.classList.add(className.toString())
 	button.setAttribute("id", id.toString())
@@ -11,20 +11,120 @@ function createButton(className, id, text, clickFunction, parent){
 	}
 }
 
+function endOfString(text){	
+		return text.charAt(text.length - 1)
+}
+
+function checkOperationsEndOfString(text){
+	switch(text.length - 1){
+		case text.indexOf("+"):
+			return true
+		case text.indexOf("-"):
+			return true
+		case text.indexOf("*"):
+	  	return true
+		case text.indexOf("/"):
+			return true
+		default:
+			return false
+	}
+	
+}
+function checkOperations(text){
+	let total = 0
+	total += text.indexOf("+")
+	total += text.indexOf("-")
+	total += text.indexOf("*")
+	total += text.indexOf("/")
+	if(total === -4){
+		return false
+	}
+	else{
+		return true
+	}
+}
+
+function interpretOperations(text){
+	undefined	
+}
+
 
 function drawButtons(){
 	const textDisplay = document.getElementById("text-display") 
 	const buttonContainer = document.getElementById("button-container")
+	let operationText = ""
 	console.log(buttonContainer)
 	//0 button is in an unusual place due to the nature of this for loop
 	for(let i = 0; i < 10; i++){
-		createButton("number-button", i, i, () => {
-			operation += i.toString()
-			operation = parseInt(operation).toString()
+		createButton("number-button", i, i, buttonContainer, () => {
+			if(i === 0 && checkOperationsEndOfString(operation)){
+				operation += ""
+			}
+			else{
+				operation += i.toString()
+			}
+			if(checkOperations(operation) === false){
+				operation = parseInt(operation).toString()
+			}
+			
 			textDisplay.innerHTML = operation
 			console.log(operation)
-		}, buttonContainer)
+		})
 	}
+	createButton("operation-button", "clear", "CE", buttonContainer, () => {
+		operation = "0"
+		textDisplay.innerHTML = operation
+	})
+
+	createButton("operation-button", "delete", "del", buttonContainer, () => {
+		operation = operation.slice(0, operation.length - 1)
+		if (operation === ""){
+			operation = "0"	
+		}
+		textDisplay.innerHTML = operation
+	})
+
+	createButton("operation-button", "add", "+", buttonContainer, () => {
+		if(checkOperations(operation) === false){
+			operation += "+"
+			operationText = operation
+			operationText += "_+_"
+		  textDisplay.innerHTML = operation
+		}	
+	})
+
+	createButton("operation-button", "subtract", "-", buttonContainer, () => {
+		if(checkOperations(operation) === false){
+			operation += "-"
+			operationText = operation
+			operationText += "_-_"
+		  textDisplay.innerHTML = operation
+		}	
+	})
+	
+	createButton("operation-button", "multiply", "*", buttonContainer, () => {
+		if(checkOperations(operation) === false){
+			operation += "*"
+			operationText = operation
+			operationText += "_*_"
+		  textDisplay.innerHTML = operation
+		}	
+	})
+
+	createButton("operation-button", "divide", "/", buttonContainer, () => {
+		if(checkOperations(operation) === false){
+			operation += "/"
+			operationText = operation
+			operationText += "_/_"
+		  textDisplay.innerHTML = operation
+		}	
+	})
+
+	createButton("operation-button", "equals", "=", buttonContainer, () => {
+		operationText += operation.substring(operation.lastIndexOf("_"), operation.length - 1)
+		console.log(operationText)
+		interpretOperations(operationText)
+	})
 
 }
 
